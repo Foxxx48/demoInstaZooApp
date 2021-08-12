fe14a1a7429fc1b8040a2c1a82dd5bd76c522ed8
Foxxx48 <foxxdim@mail.ru>
2021-08-12 17:31:40 +0300

    Commit Before work with Postman with few errors

---
+++ demo/src/main/java/com/example/demo/payload/response/MessageResponse.java
@@ -0,0 +1,13 @@
+package com.example.demo.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.security.PrivateKey;
+
+@Data
+@AllArgsConstructor
+public class MessageResponse {
+    private String message;
+
+}
---
+++ demo/src/main/java/com/example/demo/validations/EmailValidator.java
@@ -0,0 +1,32 @@
+package com.example.demo.validations;
+
+import com.example.demo.annotations.ValidEmail;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailValidator implements ConstraintValidator<ValidEmail, String> {
+    //public static final String EMAIL_PATTERN = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^-]+(?:\\\\.[a-zA-Z0-9_!#$%&’*+/=?`{|}~^-]+)*@[a-zA-Z0-9-]+(?:\\\\.[a-zA-Z0-9-]+)*$"
+    //public static final String EMAIL_PATTERN = "\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*\\.\\w{2,4}";
+    public static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" +
+            "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+    @Override
+    public void initialize(ValidEmail constraintAnnotation) {
+
+    }
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
+        return (validateEmail(email));
+    }
+
+    private boolean validateEmail(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+
+    }
+}
